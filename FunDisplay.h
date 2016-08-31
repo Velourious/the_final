84d34067824ef497bbdacff4a9c17395964c1a76
Velourious <velourious@hotmail.com>
2016-08-31 02:51:28 -0700

    Create FunDisplay.h

---
+++ FunDisplay.h
@@ -0,0 +1,25 @@
+//Class includes large title displays
+#ifndef FUNDISPLAY_H
+#define FUNDISPLAY_H
+#pragma once
+
+using namespace std;
+class FunDisplay;//FWD declaration
+
+class FunDisplay
+{
+public:
+	void splashScreen();
+	void right();
+	void wrong();
+	void bye();
+	void stats();
+
+	void add();
+	void subtract();
+	void multiply();
+	void divide();
+
+private:
+};
+#endif
